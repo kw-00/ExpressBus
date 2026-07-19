eae4d12568bacae8af3dbb6b6b49a40b7de19558
kw-00 <christoper.p.wojcik@gmail.com>
2026-07-19 23:13:22 +0200

    Create SerializationGeneration

---
+++ ExpressBus.Protocol.Sourcegen/Generation/SerializationGeneration.cs
@@ -0,0 +1,54 @@
+namespace ExpressBus.Protocol.Sourcegen.Generation;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class SerializationGeneration
+{
+    private static readonly string[] _lineSeparators = { "\r\n", "\r", "\n" };
+
+    private static string Indent(string content)
+    {
+        if (string.IsNullOrEmpty(content)) return content;
+        var sb = new StringBuilder();
+        var lines = content.Split(_lineSeparators, StringSplitOptions.None);
+        foreach (var line in lines)
+        {
+            sb.AppendLine("    " + line);
+        }
+        return sb.ToString().TrimEnd('\r', '\n');
+    }
+
+    public static string Generate(string className, IReadOnlyList<SerializablePropData> propData)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"public partial class {className}");
+        sb.AppendLine("{");
+
+        // Properties
+        foreach (var prop in propData)
+        {
+            sb.AppendLine(Indent(PropGeneration.Generate(prop)));
+        }
+        sb.AppendLine();
+
+        // Constructor
+        sb.AppendLine(Indent(ConstructorGeneration.Generate(className, propData)));
+        sb.AppendLine();
+
+        // ByteCount
+        sb.AppendLine(Indent(ByteCountGeneration.Generate(propData)));
+        sb.AppendLine();
+
+        // ToBytes
+        sb.AppendLine(Indent(ToBytesGeneration.Generate(className, propData)));
+        sb.AppendLine();
+
+        // FromBytes
+        sb.AppendLine(Indent(FromBytesGeneration.Generate(className, propData)));
+        sb.AppendLine("}");
+
+        return sb.ToString();
+    }
+}
